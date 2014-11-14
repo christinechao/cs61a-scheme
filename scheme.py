944b85bc59a8f1646f8b831ef2869ad549292753
sirspinach <wang.steven.h@gmail.com>
2014-11-13 19:54:17 -0800

    finish q05A

--- scheme.py
+++ scheme.py
@@ -210,7 +210,9 @@ def do_define_form(vals, env):
     target = vals[0]
     if scheme_symbolp(target):
         check_form(vals, 2, 2)
-        "*** YOUR CODE HERE ***"
+        value = scheme_eval(vals[1], env)
+        env.define(target, scheme_eval(vals[1], env))
+        return target
     elif isinstance(target, Pair):
         "*** YOUR CODE HERE ***"
     else:
